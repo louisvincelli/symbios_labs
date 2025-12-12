83fbda21861ecdb6957fbfe9c48f1f9e6c4df9f4
Louis Vincelli <louisvincelli@gmail.com>
2025-12-11 21:19:37 -0500

    projects section
    projects

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero/Hero';
 import Research from './Research/Research';
 import Philosophy from './Philosophy/Philosophy';
+import Projects from './Projects/Projects';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Research />
       <Philosophy />
+      <Projects />
     </div>
   );
 };
--- components/Home/Philosophy/Philosophy.tsx
+++ components/Home/Philosophy/Philosophy.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import { FaCheckCircle } from 'react-icons/fa';
 
 const Philosophy = () => {
   return (
@@ -12,9 +13,12 @@ const Philosophy = () => {
             </div>
             {/* Text Content */}
             <div className='p-6'>
-                <h1 className='text-base font-semibold text-orange-500'>Philosophy</h1>
+                <h1 className='text-2xl font-semibold text-orange-500'>Philosophy</h1>
                 <h1 className='mt-4 text-xl sm:text-2xl md:text-3xl font-bold text-gray-900'>Symbios Labs studies the interdependence between human cognition and artificial systems &#8212; Advancing empathy, ethics, and intelligence through co-evolution.</h1>
-                <h1 className='mt-4 text-md sm:text-xl md:text-xl font-bold text-gray-700'>Evolving together. &#8226; The science of shared intelligence. &#8226; Where humanity and AI grow in harmony.</h1>
+                <h1 className='mt-4 text-md sm:text-xl md:text-xl font-bold text-gray-700'>Evolving together &#8226; The science of shared intelligence &#8226; Where humanity and AI grow in harmony</h1>
+                <p className='mt-4 text-gray-600 text-sm sm:text-base md:text-lg'>At Symbios Labs, we believe in the power of co-evolution. Our research explores how human cognition and artificial systems can enhance each other, leading to a future where empathy, ethics, and intelligence thrive together.</p>
+                <p className='mt-4 text-gray-600 text-sm sm:text-base md:text-lg'>Join us in our mission to create a world where living and artificial intelligence evolve in harmony, shaping a future that benefits all.</p>
+                <button className='mt-8 px-8 py-3 bg-gray-100 text-gray-800 font-semibold rounded-full hover:bg-teal-500 transition all duration-200 hover:text-white'>Explore our Philosophy &rarr;</button>
             </div>
         </div>
     </div>
